6a3b4f84b1102a3ca155e70bbe7f1c347148640e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-27 08:58:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-//const getInfoYaoi = require('./functions/info-yaoi')
 const getRandomImage = require('./functions/info-random-image')
 
 module.exports = {
-//getInfoYaoi,
-getRandomImage,
+getRandomImage
 }
