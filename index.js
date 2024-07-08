4d198433d909fc2d77f4b4235d8bb16be9d72583
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-08 17:03:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const getRandomImage = require('./functions/info-random-image')
 const getRandomImageBoys = require('./functions/random_boys')
+const getRandomImageNsfw = require('./functions/random_nsfw.js')
 
 module.exports = {
 getRandomImage,
-getRandomImageBoys
+getRandomImageBoys,
+getRandomImageNsfw
 }
